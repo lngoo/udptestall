1439b40d3920a2c1bb54970cb4a810effffd4b45
lngoo <lgl123ok@126.com>
2019-11-05 23:03:34 +0800

    Initial commit

---
+++ udp-base/src/main/java/UdpClient.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.net.*;
+
+/***
+ * UDP Client端
+ ***/
+public class UdpClient {
+
+    private String sendStr = "hello";
+    private String netAddress = "255.255.255.255";
+    private final int PORT = 5060;
+
+    private DatagramSocket datagramSocket;
+    private DatagramPacket datagramPacket;
+
+    public UdpClient(){
+        try {
+            datagramSocket = new DatagramSocket();
+            byte[] buf = sendStr.getBytes();
+            InetAddress address = InetAddress.getByName(netAddress);
+            datagramPacket = new DatagramPacket(buf, buf.length, address, PORT);
+            datagramSocket.send(datagramPacket);
+
+            byte[] receBuf = new byte[1024];
+            DatagramPacket recePacket = new DatagramPacket(receBuf, receBuf.length);
+            datagramSocket.receive(recePacket);
+
+            String receStr = new String(recePacket.getData(), 0 , recePacket.getLength());
+            
+            //获取服务端ip
+            String serverIp = recePacket.getAddress().getHostAddress();
+            System.out.println(serverIp + "/// " + receStr);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭socket
+            if(datagramSocket != null){
+                datagramSocket.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    UdpClient udpClient = new UdpClient();
+                }
+            }).start();
+        }
+    }
+}
---
+++ udp-netty/src/main/java/Client.java
@@ -0,0 +1,54 @@
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.util.CharsetUtil;
+
+import java.net.InetSocketAddress;
+
+
+public class Client {
+
+    private static int scanPort = 2555;
+
+    public Client(int scanPort) {
+        this.scanPort = scanPort;
+    }
+
+    public static void main(String[] args) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioDatagramChannel.class)
+                    .handler(new ClientHandler());
+
+            Channel ch = b.bind(0).sync().channel();
+
+            ch.writeAndFlush(new DatagramPacket(
+                    Unpooled.copiedBuffer("(来自客户端:请帮我注册)", CharsetUtil.UTF_8),
+                    new InetSocketAddress("127.0.0.1", scanPort))).sync();
+
+            ch.closeFuture().await();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    private static class ClientHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket packet) throws Exception {
+            String body = packet.content().toString(CharsetUtil.UTF_8);
+            System.out.println("客户端收到信息：" + body);
+        }
+    }
+}
